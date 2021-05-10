d9e6d534407c466a57d7b52a002e4d2140ec3ddf
catakalina <79998936+catakalina@users.noreply.github.com>
2021-05-10 17:04:30 -0500

    index.php

---
+++ views/partials/footer.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> <?= date('Y') ?>
+    </div>
+    <strong>Copyright &copy; <?= date('Y') ?> <a href="<?= $baseURL; ?>"><?= $_ENV['ALIASE_SITE'] ?></a>.</strong>
+    Derechos Reservados.
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
---
+++ views/partials/routes.php
@@ -0,0 +1,18 @@
+<?php
+use App\Models\GeneralFunctions;
+//$RutaAbsoluta = "\WebER\views\index.php"; //https://www.php.net/manual/es/regexp.reference.escape.php
+//$RutaRelativa = "../index.php";
+
+//Carga las librerias importadas del composer
+require(__DIR__ .'/../../vendor/autoload.php');
+//__DIR__ => D:\laragon\www\WebER\views\partials
+if(GeneralFunctions::loadEnv(['ROOT_FOLDER'])){
+    $baseURL = $_SERVER['REQUEST_SCHEME']."://".$_SERVER['HTTP_HOST']."/".$_ENV['ROOT_FOLDER'];
+//https://localhost/Proyecto/
+    $adminlteURL = $baseURL."/vendor/almasaeed2010/adminlte";
+//https://localhost/Proyecto/vendor/almasaeed2010/adminlte
+}else{
+    GeneralFunctions::logFile('Error al cargar variables del entorno');
+    die();
+}
+?>
